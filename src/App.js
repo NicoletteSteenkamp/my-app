b6716c1c662bf2499b1f736116b8c43315cdbb02
Nicolette <steenkampnicolette815@gmail.com>
2023-10-13 11:38:27 +0200

    added styling and body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import "./App.css";
+
 import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather defaultCity="Boksburg" />
+      <div className="container">
+        <Weather defaultCity="Boksburg" />
+        <footer>
+          This app is coded by Nicolette Steenkamp and the open source code is
+          here on{" "}
+          <a
+            href="https://github.com/NicoletteSteenkamp/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and is deployed here{" "}
+          <a
+            href="https://gilded-kringle-0cc646.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    const { city, condition, temperature, wind, time } = response.data;
+
+    setWeatherData({
+      ready: true,
+      city: city,
+      condition: condition.description,
+      icon: condition.icon_url,
+      temperature: temperature.current,
+      humidity: temperature.humidity,
+      windSpeed: wind.speed,
+      date: new Date(time * 1000).toLocaleString(),
+    });
+  }
+
+  useEffect(() => {
+    const apiKey = "ae997t30869fc345038bf7f0abaao7e6";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }, [props.defaultCity]);
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter City"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-light w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.condition}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src={weatherData.icon}
+                alt={weatherData.condition}
+                className="float-left"
+              />
+              <div className="float-left">
+                <span className="temperature">
+                  {" "}
+                  {Math.round(weatherData.temperature)}
+                </span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Windspeed: {Math.round(weatherData.windSpeed)} km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return "Loading...";
+  }
+}
