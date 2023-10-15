aa3f6542e7e2dc0e27181d5f6643e4bce6b41559
Nicolette <steenkampnicolette815@gmail.com>
2023-10-15 16:15:51 +0200

    added a missing file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import 'semantic-ui-css/semantic.min.css'
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.css";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-reportWebVitals();
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
